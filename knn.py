e7086a2e98399c8b213a6850e654b5a34f204311
Matthew <55106609+IskornevME@users.noreply.github.com>
2022-10-23 23:00:48 +0300

    added simple method to count distance (2 loops)

--- knn.py
+++ knn.py
@@ -9,3 +9,12 @@ class KNearestNeighbor:
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
+
+    def compute_distances_two_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            for j in range(num_train):
+                dists[i][j] = np.sqrt(sum((X[i] - self.X_train[j]) ** 2))
+        return dists
